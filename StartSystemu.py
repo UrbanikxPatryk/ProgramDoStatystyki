647b9b04ad8586e0b41535f927275f22e287ce97
UrbanikPatryk <patrykxurbanik@gmail.com>
2023-05-28 14:49:46 +0200

    zbudowanie struktur przejscia do menu

--- StartSystemu.py
+++ StartSystemu.py
@@ -16,7 +16,7 @@ def kotrola(User_name_input):
    if len(User_name_input)==0:
        return False  
 
-   elif len(User_name_input)<2 :
+   elif len(User_name_input) < 2 :
        return False
 
    else :
@@ -24,14 +24,31 @@ def kotrola(User_name_input):
 
 
 def odpowiedzi(User_name_input,Boot):
-
+   
    if kotrola(User_name_input)==True:
-       print(f"{Boot} > Witam cie {User_name_input} zapraszam cie do programu i zycze ci milego uzywania tego oprogramownia  ")
+       print(f"{Boot} > Witam cie {User_name_input} zapraszam cie do programu i zycze ci milego uzywania tego oprogramownia\n")
+       przejscie(User_name_input,Boot)
 
    else:
        print(f"{Boot} > Przytkor mi bardzo ze nie poznalem towojego imienia moze :( lecz licze  ze podasz go  nastepnym razem )")
 
-  
+def przejscie(User_name_input,Boot):
+    try: 
+        Button_Input=int(input(f"{Boot} > Kliknij Key 1 By wejść do Menu: "))
+    except ValueError :
+        os.system("cls")
+        print(f"{Boot} > Coś poszło nie tak sproboj jeszcze raz {User_name_input} ")
+        przejscie(User_name_input,Boot)
+         
+    else:
+        match Button_Input : 
+            case 1 : 
+                os.system("cls")
+                print("W Budowanie ")
 
+            case _ : 
+                os.system("cls")
+                print(f"{Boot} > Ups Chyba  nie ma takiego klawisza nic nie szkodzi {User_name_input} zdarza sie sproboj jeszcze raz ")
+                przejscie(User_name_input,Boot)
 
 menu()    
